10fcc91c5a22b3c899f5e23769fcb9238f4735d1
Lucas Amaral <analista.sistemas.lucas@gmail.com>
2023-11-28 14:27:50 +0100

    Component Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
+import { Header } from './components/Header'
+import './global.css'
 
 function App() {
 
   return (
-    <h1>Hello world</h1>
+    <div>
+      <Header />
+
+      <div className='wrapper'></div>
+    </div>
   )
 }
 
