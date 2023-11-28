8a76c740f95efaee8b2d9fa144ed82be8fa809d2
Lucas Amaral <analista.sistemas.lucas@gmail.com>
2023-11-28 16:26:03 +0100

    sidebar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
 import { Header } from './components/Header'
 import './global.css'
 
+import styles from './App.module.css'
+import { Sidebar } from './components/Sidebar'
+
 function App() {
 
   return (
     <div>
       <Header />
 
-      <div className='wrapper'></div>
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          posts
+        </main>
+      </div>
     </div>
   )
 }
