a958211f1f3781a0ae2db1e2ba3ca7be11b5eb1b
Lucas Amaral <analista.sistemas.lucas@gmail.com>
2023-11-30 03:15:12 +0100

    Props of Post

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,37 @@ import { Sidebar } from './components/Sidebar'
 
 import './global.css'
 
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://avatars.githubusercontent.com/u/1681405?v=4',
+      name: 'Chloe Baitard',
+      role: 'Manager @SportsConnect'
+    },
+    content: [
+      { type: 'paragraph', content: 'Hey guys 👋'},
+      { type: 'paragraph', content: 'This is a comment on an example of a form Im making for a portfolio of basic React functions 🚀'},
+      { type: 'link', content: 'lucas.design/sportsconnect'}
+    ],
+    publishedAt: new Date('2023-11-29 18:46:00')
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://avatars.githubusercontent.com/u/63449524?v=',
+      name: 'Lucas Amaral',
+      role: 'CTO @SportsConnect'
+    },
+    content: [
+      { type: 'paragraph', content: 'Hey guys 👋'},
+      { type: 'paragraph', content: 'This is a comment on an example of a form Im making for a portfolio of basic React functions 🚀'},
+      { type: 'link', content: 'lucas.design/sportsconnect'}
+    ],
+    publishedAt: new Date('2023-11-05 18:46:00')
+  },
+]
+
 import styles from './App.module.css'
 
 function App() {
@@ -15,8 +46,17 @@ function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <Post />
-          <Post />
+          {
+            posts.map(post => {
+              return (
+                <Post 
+                  author={post.author}
+                  content={post.content}
+                  publishedAt={post.publishedAt}
+                />
+              )
+            })
+          }
         </main>
       </div>
     </div>
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,32 +1,43 @@
+import { format, formatDistanceToNow } from 'date-fns'
+import ptBR from 'date-fns/locale/pt-BR'
 import { Avatar } from './Avatar'
 import { Comment } from './Comment'
 import styles from './Post.module.css'
 
-export function Post() {
+export function Post({author, publishedAt, content}) {
+
+    const publishedDateFormatted = format(publishedAt, "d 'de' LLLL 'as' HH:mm", {
+        locale: ptBR
+    })
+
+    const publishedDateRelativeToNow = formatDistanceToNow(publishedAt,{
+        locale: ptBR,
+        addSuffix: true
+    })
     return (
         <article className={styles.post}>
             <header>
                 <div className={styles.author}>
-                    <Avatar src="https://avatars.githubusercontent.com/u/63449524?v=4" />
+                    <Avatar src={author.avatarUrl} />
                     <div className={styles.authorInfo}>
-                        <strong>Lucas Amaral</strong>
-                        <span>Software Engineer</span>
+                        <strong>{author.name}</strong>
+                        <span>{author.role}</span>
                     </div>
                 </div>
-                <time title='28 de Novembro as 18:21' dateTime='2023-11-28 18:21:11'>Published 1 hour ago</time>
+                <time title={ publishedDateFormatted } dateTime={publishedAt.toISOString()}>
+                {publishedDateRelativeToNow}    
+                </time>
             </header>
             <div className={styles.content}>
-                <p>Hey guys 👋</p>
-
-                <p>This is a comment on an example of a form I'm making for a portfolio of basic React functions 🚀</p>
-
-                <p><a href="#">jane.design/doctorcare</a></p>
-
-                <p>
-                    <a href="#">#novoprojeto</a>{' '}
-                    <a href='#'>#nlw</a>{' '}
-                    <a href='#'>#rocketseat</a>{' '}
-                </p>
+                {
+                    content.map(line => {
+                        if(line.type === 'paragraph'){
+                            return <p>{line.content}</p>
+                        }else if( line.type === 'link'){
+                            return <p><a href='#'>{line.content}</a></p>
+                        }
+                    })
+                }
             </div>
 
             <form className={styles.commentForm}>
